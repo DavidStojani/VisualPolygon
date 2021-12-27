8fe45a12fde5a71eabc45c393470c8304787de3b
marcel <mt@bingooo.com>
2021-12-28 00:02:58 +0100

    Bindings

--- src/main/java/com/bachelor/visualpolygon/view/ViewController.java
+++ src/main/java/com/bachelor/visualpolygon/view/ViewController.java
@@ -1,25 +1,78 @@
 package com.bachelor.visualpolygon.view;
 
 import com.bachelor.visualpolygon.viewmodel.ViewModel;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.StrokeLineCap;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewController {
+
+    @FXML
+    public AnchorPane pane;
     @FXML
     private Label statusText;
 
+    private ViewModel viewModel;
+
+    ObservableList<Double> x = FXCollections.observableArrayList();
+    ObservableList<Double> y = FXCollections.observableArrayList();
+    private ListProperty<Double> xCoordinates = new SimpleListProperty<>();
+    private ListProperty<Double> yCoordinates = new SimpleListProperty<>();
+    Group root = new Group();
+
+
+    EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            if (mouseEvent.getTarget().toString().contains("Anchor") && mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED) {
+
+                x.add(mouseEvent.getX());
+                y.add(mouseEvent.getY());
+                System.out.println(mouseEvent.getX()+"und"+mouseEvent.getY());
+                root.getChildren().add(new Circle(mouseEvent.getX(),mouseEvent.getY(),4));
+
+            }
+        }
+    };
 
 
     public void init(ViewModel viewModel) {
         System.out.println("Works");
+        xCoordinates.setValue(x);
+        yCoordinates.setValue(y);
+        pane.setOnMouseClicked(mouseHandler);
+        pane.getChildren().add(root);
     }
 
-    public void resetApplicationAction(ActionEvent actionEvent) {
+    public void resetApplication() {
         statusText.setText("RESETTING");
+        root.getChildren().clear();
+        x.clear();
+        y.clear();
     }
 
-    public void updateStatus(ActionEvent actionEvent) {
-        statusText.setText("UPDATING");
+    public void updateStatus() {
+
     }
+
+
+
 }
+
--- src/main/java/com/bachelor/visualpolygon/viewmodel/ViewModel.java
+++ src/main/java/com/bachelor/visualpolygon/viewmodel/ViewModel.java
@@ -1,12 +1,20 @@
 package com.bachelor.visualpolygon.viewmodel;
 
 import com.bachelor.visualpolygon.model.DataModel;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+
+import java.util.ArrayList;
 
 public class ViewModel {
 
     private DataModel model;
+    private ListProperty<Double> xCoordinate = new SimpleListProperty<>();
+    private ListProperty<Double> yCoordinate = new SimpleListProperty<>();
 
     public ViewModel(DataModel model){
         this.model =model;
     }
+
+
 }
